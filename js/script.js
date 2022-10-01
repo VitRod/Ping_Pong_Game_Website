051001f7b7823595e4535ea2434919b0da8efb2e
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-01 15:32:41 +0300

    // Canvas const { body } = document; const canvas = document.createElement('canvas'); const context = canvas.getContext('2d'); const width = 500; const height = 700; const screenWidth = window.screen.width; const canvasPosition = screenWidth / 2 - width / 2; const isMobile = window.matchMedia('(max-width: 600px)'); const gameOverEl = document.createElement('div');

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Canvas
+const { body } = document;
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+const width = 500;
+const height = 700;
+const screenWidth = window.screen.width;
+const canvasPosition = screenWidth / 2 - width / 2;
+const isMobile = window.matchMedia('(max-width: 600px)');
+const gameOverEl = document.createElement('div');
